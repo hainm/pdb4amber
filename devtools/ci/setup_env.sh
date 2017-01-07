8b061c864606054fa2b9e496fb5de9e67898d7b3
Hai Nguyen <hainm.comp@gmail.com>
2017-01-07 15:19:50 -0500

    Miniconda3

--- devtools/ci/setup_env.sh
+++ devtools/ci/setup_env.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh;
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
 else
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 fi
 
 bash miniconda.sh -b
 
-export PATH=$HOME/miniconda/bin:$PATH
+export PATH=$HOME/miniconda3/bin:$PATH
 # install stable version
 
 # create myenv
