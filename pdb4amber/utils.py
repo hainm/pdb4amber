c44004fb2e19a87394f958c3ed46d84ecb3d52f7
Hai Nguyen <hainm.comp@gmail.com>
2017-01-21 00:21:54 -0500

    remove "which"

--- pdb4amber/utils.py
+++ pdb4amber/utils.py
@@ -14,23 +14,6 @@ def tempfolder():
   os.chdir(cwd)
   rmtree(my_temp)
 
-# copied from $AMBERHOME/updateutils/utils.py
-def which(program):
-   """ Looks for a program in the PATH """
-   def is_exe(prog):
-      return os.path.exists(prog) and os.access(prog, os.X_OK)
-
-   # See if program has a full path. If it is, do not search PATH
-   fpath, fname = os.path.split(program)
-   if fpath:
-      return is_exe(program)
-
-   for d in os.environ['PATH'].split(os.pathsep):
-      if is_exe(os.path.join(d, program)):
-         return os.path.join(d, program)
-
-   return None
-
 def amberbin(program_str):
     amberhome = os.environ.get('AMBERHOME', '')
     program = os.path.join(amberhome, 'bin', program_str)
