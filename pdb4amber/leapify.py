0aa2c1526580f45691c889ba792db26443438ee3
Hai Nguyen <hainm.comp@gmail.com>
2017-01-26 23:42:45 -0500

    delay importing scipy and pysander

--- pdb4amber/leapify.py
+++ pdb4amber/leapify.py
@@ -1,10 +1,11 @@
 from .pdb4amber import AmberPDBFixer
-from parmed.tools.simulations import sanderapi
 from .utils import tempfolder
 from .leap_runner import run_tleap
 
 
 class Leapify(AmberPDBFixer):
+    from parmed.tools.simulations import sanderapi
+
     def minimize(self, *args, **kwargs):
         sanderapi.minimize(self.parm, *args, **kwargs)
 
