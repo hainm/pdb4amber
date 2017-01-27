00abf2e201cea5a76748d56f9541cb8fde06ff10
Hai Nguyen <hainm.comp@gmail.com>
2017-01-27 00:31:40 -0500

    fix template typos

--- pdb4amber/leap_runner.py
+++ pdb4amber/leap_runner.py
@@ -1,10 +1,9 @@
 import subprocess
 import parmed
-
 from .template import default_force_field, leap_template
 
 
-def run_tleap(parm, ns_names, gaplist, sslist, forcefield_cmd=''):
+def run_tleap(parm, ns_names, gaplist, sslist, forcefield_cmd=None, leap_input=None):
     # adapted from amber_adaptbx code in phenix
     '''
 
@@ -14,7 +13,7 @@ def run_tleap(parm, ns_names, gaplist, sslist, forcefield_cmd=''):
     ns_names : List[str]
     gaplist : List[int]
     sslist : List[Tuple[int, int]]
-    forcefield_cmd : str, optional
+    forcefield_cmd : str or None, optional
         If given, use  this for force fields assignment
     '''
     input_pdb = 'x.pdb'
@@ -26,40 +25,44 @@ def run_tleap(parm, ns_names, gaplist, sslist, forcefield_cmd=''):
     tleap_input_file = "leap.in"
     f = open(tleap_input_file, "w")
 
-    # box
-    box = parm.box
-    if box is not None:
-        box_info = 'set x box { %s  %s  %s }' % (box[0], box[1], box[2])
+    if leap_input is None:
+        # box
+        box = parm.box
+        if box is not None:
+            box_info = 'set x box { %s  %s  %s }' % (box[0], box[1], box[2])
+        else:
+            box_info = ''
+
+        # Now we can assume that we are dealing with AmberTools16:
+        more_force_fields = ''
+        if forcefield_cmd is not None:
+            for res in ns_names:
+                more_force_fields += '%s = loadmol2 %s.mol2\n' % (res, res)
+                more_force_fields += 'loadAmberParams %s.frcmod\n' % res
+
+        #  more_leap_cmds 
+        more_leap_cmds = ''
+        if gaplist:
+            for d, res1, resid1, res2, resid2 in gaplist:
+                more_leap_cmds += 'deleteBond x.%d.C x.%d.N\n' % (resid1, resid2)
+
+        #  process sslist
+        if sslist:
+            for resid1, resid2 in sslist:
+                more_leap_cmds += 'bond x.%d.SG x.%d.SG\n' % (resid1, resid2)
+
+        leap_string = leap_template.format(
+            force_fields=default_force_field,
+            more_force_fields=forcefield_cmd,
+            box_info=box_info,
+            input_pdb=input_pdb,
+            prmtop=prmtop,
+            rst7=rst7,
+            more_leap_cmds=more_leap_cmds)
     else:
-        box_info = ''
-
-    # Now we can assume that we are dealing with AmberTools16:
-    more_force_fields = ''
-    if not forcefield_cmd:
-        for res in ns_names:
-            more_force_fields += '%s = loadmol2 %s.mol2\n' % (res, res)
-            more_force_fields += 'loadAmberParams %s.frcmod\n' % res
-
-    #  more_leap_cmds 
-    more_leap_cmds = ''
-    if gaplist:
-        for d, res1, resid1, res2, resid2 in gaplist:
-            more_leap_cmds += 'deleteBond x.%d.C x.%d.N\n' % (resid1, resid2)
-
-    #  process sslist
-    if sslist:
-        for resid1, resid2 in sslist:
-            more_leap_cmds += 'bond x.%d.SG x.%d.SG\n' % (resid1, resid2)
-
-    leap_string = leap_template.format(
-        force_fields=default_force_field,
-        more_force_fields=forcefield_cmd,
-        box_info=box_info,
-        input_pdb=input_pdb,
-        prmtop=prmtop,
-        rst7=rst7,
-        more_leap_cmds=more_leap_cmds)
-
+        leap_string = leap_input.format(input_pdb=input_pdb,
+                prmtop=prmtop,
+                rst7=rst7)
     f.write(leap_string)
     f.close()
 
--- tests/test_leapify.py
+++ tests/test_leapify.py
@@ -1,5 +1,6 @@
 import parmed as pmd
 from pdb4amber import Leapify
+from pdb4amber.leap_runner import run_tleap
 
 from utils import get_fn
 
@@ -18,3 +19,23 @@ def test_minimization():
             maxcyc=2,
             disp=False,
             callback=callback)
+
+leap_input = """
+source leaprc.protein.ff14SB
+source leaprc.water.tip3p
+pdb = loadpdb {input_pdb}
+solvateBox pdb TIP3PBOX 15
+saveamberparm pdb {prmtop} {rst7}
+"""
+def test_run_tleap():
+    pdb_fn = get_fn('2igd/2igd.pdb')
+    parm = pmd.load_file(pdb_fn)
+    parm.strip(':HOH')
+    print(parm)
+
+    new_parm = run_tleap(parm, ns_names=[],
+            gaplist=[],
+            sslist=[],
+            leap_input=leap_input)
+    assert len(new_parm.atoms) == 22884
+    assert 'WAT' in set(res.name for res in new_parm.residues)
