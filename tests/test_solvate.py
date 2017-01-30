de563bd68dda5c6fa88bc213d0fb5d9e9af2c1dd
Hai Nguyen <hainm.comp@gmail.com>
2017-01-30 14:49:48 -0500

    update test

--- tests/test_solvate.py
+++ tests/test_solvate.py
@@ -1,7 +1,7 @@
-from jamber.builder import solvate, build_protein
+from pdb4amber.builder.pytraj_build import solvate, build_protein
 
 def test_solvate():
-    traj = build_protein('ALA ALA', ['alpha:1-2'])
-    print(traj)
-    traj2 = solvate(traj)
-    assert traj2.n_atoms == 1092
+    parm = build_protein('ALA ALA', ['alpha:1-2'])
+    print(parm)
+    parm2 = solvate(parm)
+    assert len(parm2.atoms) == 1092
