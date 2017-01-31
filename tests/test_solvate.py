6086dac4da663949c801ee831081d87e6531abb1
Hai Nguyen <hainm.comp@gmail.com>
2017-01-31 11:16:23 -0500

    update test

--- tests/test_solvate.py
+++ tests/test_solvate.py
@@ -2,6 +2,5 @@ from pdb4amber.builder.pytraj_build import solvate, build_protein
 
 def test_solvate():
     parm = build_protein('ALA ALA', ['alpha:1-2'])
-    print(parm)
     parm2 = solvate(parm)
     assert len(parm2.atoms) == 1092
