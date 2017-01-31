cd82727ce05508953f06152905b9b48364f21314
Hai Nguyen <hainm.comp@gmail.com>
2017-01-31 17:17:20 -0500

    add _run_antechamber_ccif (from amber_adaptbx

--- pdb4amber/leapify.py
+++ pdb4amber/leapify.py
@@ -1,6 +1,7 @@
 from .pdb4amber import AmberPDBFixer
-from .utils import tempfolder
+from .utils import tempfolder, which
 from .leap_runner import run_tleap
+import subprocess
 
 
 class Leapify(AmberPDBFixer):
@@ -41,4 +42,28 @@ class Leapify(AmberPDBFixer):
             gaplist = self.find_gaps() if self.parm is not None else []
             sslist, _ = self.find_disulfide() if self.parm is not None else []
             self.parm = run_tleap(self.parm, ns_names, gaplist, sslist,
-                    *args, **kwargs)
+                                  *args, **kwargs)
+
+    def _run_antechamber_ccif(self, residue_name, use_am1_and_maxcyc_zero=True):
+        # Original code from amber_adaptbx
+        '''
+        run antechamber from a components.cif file:
+        '''
+        if not which('antechamber'):
+            raise OSError("Make sure to have antechamber")
+        mask = ':' + residue_name
+        ccif = 'test.mol2'
+        self.parm[mask][':1'].save(ccif, overwrite=True)
+        cmds = []
+        cmd = 'antechamber -i %s -fi mol2 -bk %s -o %s.mol2 -fo mol2 \
+          -s 2 -pf y -c bcc -at gaff2' % (ccif, residue_name, residue_name)
+        if use_am1_and_maxcyc_zero:
+            cmd += ' -ek "qm_theory=\'AM1\', grms_tol=0.0005, scfconv=1.d-10, maxcyc=0, ndiis_attempts=700,"'
+        cmds.append(cmd)
+
+        for cmd in cmds:
+            output = subprocess.check_output(cmd, shell=True)
+
+        cmd = 'parmchk2 -s 2 -i %s.mol2 -f mol2 -o %s.frcmod' % (residue_name,
+                                                                 residue_name)
+        subprocess.check_call(cmd, shell=True)
