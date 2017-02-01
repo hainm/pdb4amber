325156b534e94dc6cc307b08f28da9dc99cd8ec6
Hai Nguyen <hainm.comp@gmail.com>
2017-02-01 13:40:53 -0500

    test for amber_builder

---
+++ tests/test_amber_builder.py
@@ -0,0 +1,41 @@
+import unittest
+from numpy.testing import assert_almost_equal as aa_eq
+from pdb4amber.amber_builder import AmberBuilder
+from pdb4amber.utils import tempfolder
+
+try:
+    import pytraj
+except ImportError:
+    pytraj = None
+
+def _compute_major_groove(builder):
+    with  tempfolder():
+        pdb_out = 'out.pdb'
+        builder.write_pdb(pdb_out)
+        traj = pytraj.load(pdb_out)
+
+        data = pytraj.nastruct(traj)
+        return data.major[1][0]
+
+
+@unittest.skipUnless(pytraj is not None, "require pytraj")
+def test_solvate():
+    builder = AmberBuilder()
+    builder.build_protein('ALA ALA', ['alpha:1-2'])
+    builder.solvate()
+    assert len(builder.parm.atoms) == 1092
+
+@unittest.skipUnless(pytraj is not None, "require pytraj")
+def test_build_nuleic_acid():
+    builder = AmberBuilder()
+    builder.build_bdna('GGGGGG')
+    aa_eq(_compute_major_groove(builder), [ 17.24558067])
+    aa_eq(_compute_major_groove(builder.build_adna('GGGGGG')), [ 15.72079277])
+
+    # build_xxx will repalce old structure
+    aa_eq(_compute_major_groove(builder.build_arna('GGGGGG')), [ 15.18226528])
+    with tempfolder():
+        pdb_out = 'out.pdb'
+        builder.write_pdb(pdb_out)
+        with open(pdb_out) as fh:
+            assert "O2'" in fh.read()
--- tests/test_solvate.py
+++
@@ -1,7 +0,0 @@
-from pdb4amber.amber_builder import AmberBuilder
-
-def test_solvate():
-    builder = AmberBuilder()
-    builder.build_protein('ALA ALA', ['alpha:1-2'])
-    builder.solvate()
-    assert len(builder.parm.atoms) == 1092
